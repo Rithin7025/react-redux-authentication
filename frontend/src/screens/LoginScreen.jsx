ec208ca3f2b01c904211f06a690e90f0018bbce1
Rithin <rithinrj2001@gmail.com>
2023-09-20 12:19:16 +0530

    added a loader component

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -6,6 +6,7 @@ import {useDispatch,useSelector} from 'react-redux'
 import { useLoginMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import {toast} from 'react-toastify'
+import Loader from '../components/Loader';
 
 const LoginScreen = () =>{
 
@@ -51,6 +52,8 @@ const LoginScreen = () =>{
                     <Form.Label>Password</Form.Label>
                     <Form.Control placeholder='Enter password' type='password' value={password} onChange={(e)=> setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
+
+                {isLoading && <Loader/>}
 
                 <Button type='submit' variant='primary' className='mt-3'>
                     Sign In
