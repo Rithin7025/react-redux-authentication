b8e5a8449ddb083d0c1df70b8d0e1d85d833f12b
Rithin <rithinrj2001@gmail.com>
2023-09-20 11:56:26 +0530

    Added Logout functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,39 +1,69 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
-import {FaSignInAlt,FaSignOutAlt} from 'react-icons/fa'
-import {LinkContainer} from 'react-router-bootstrap'
+import { Container, Nav, Navbar, NavDropdown, Badge } from "react-bootstrap"
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa"
+import { useSelector, useDispatch } from "react-redux"
+import { LinkContainer } from "react-router-bootstrap"
+import { useLogoutMutation } from "../slices/usersApiSlice"
+import {logout} from '../slices/authSlice'
+import {useNavigate} from 'react-router-dom'
+const Header = () => {
+  const { userInfo } = useSelector((state) => state.auth)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const [logoutApiCall] = useLogoutMutation();
 
-
- const Header = () => {
-   return (
-     <header>
-      <Navbar bg ='dark' variant='dark' expand='lg' collapseOnSelect>
+  const logoutHandler = async () =>{
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/')
+    } catch (error) {
+      console.log(err)
+    }
+  }
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-          <Navbar.Brand >Mern Auth</Navbar.Brand> 
+          <LinkContainer to="/">
+            <Navbar.Brand>Mern Auth</Navbar.Brand>
           </LinkContainer>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id='basic-navbar-nav'>
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <LinkContainer to='/login'>
-              <Nav.Link >
-                <FaSignInAlt />Sign In 
-              </Nav.Link>
-            </LinkContainer>
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='userName'>
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                      <NavDropdown.Item onClick={ logoutHandler }>Logout</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/login">
+                    <Nav.Link>
+                      <FaSignInAlt />
+                      Sign In
+                    </Nav.Link>
+                  </LinkContainer>
 
-            <LinkContainer to='/register'>
-              <Nav.Link >
-                <FaSignOutAlt />Sign Up 
-              </Nav.Link>
-            </LinkContainer>
+                  <LinkContainer to="/register">
+                    <Nav.Link>
+                      <FaSignOutAlt />
+                      Sign Up
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
-
         </Container>
       </Navbar>
-     </header>
-   )
- }
- 
- export default Header
+    </header>
+  )
+}
+
+export default Header
